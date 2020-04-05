99dd360ab78f38313cb4c0994b3d60db4c2f9b2b
Guillaume <noel.guillaume@live.fr>
2020-04-05 08:59:40 +0200

    displaying Products from Contentful, with pagination

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -2,14 +2,42 @@
   <Layout>
     <h1>Produts</h1>
     <h2>
+      <div
+        style="text-align: center;"
+        v-for="edge in $page.allContentfulProduct.edges"
+        :key="edge.node.id"
+      >
+        <h2 style="margin-bottom: 0.25em;">{{edge.node.name}}</h2>
+        <span>{{edge.node.price}}</span>
+        <g-image
+          :src="edge.node.image.file.url"
+          style="width: 100%; height: 300px; object-fit: contain"
+          :alt="edge.node.image.title"
+        />
+        <p>{{edge.node.description}}</p>
+      </div>
       <g-link class="nav__link" to="/new">New Products</g-link>
     </h2>
+    <Pager :info="$page.allContentfulProduct.pageInfo" linkClass="pager" />
   </Layout>
 </template>
 
+<page-query>
+query ($page: Int)
+{allContentfulProduct (perPage: 2, page: $page) @paginate { 
+  pageInfo {
+    totalPages, currentPage
+  }
+  edges { node { id, name, price, description, image {title, file {url}}}} }}
+</page-query>
+
 <script>
+import { Pager } from "gridsome";
 // to know more about meta infos https://github.com/nuxt/vue-meta
 export default {
+  components: {
+    Pager
+  },
   metaInfo: {
     title: "Products",
     meta: [
@@ -24,3 +52,11 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.pager {
+  font-size: 1.5rem;
+  letter-spacing: 0.5px;
+  padding: 40px 20px;
+}
+</style>
